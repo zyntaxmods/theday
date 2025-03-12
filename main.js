88c3c243fe6d8e392d5b32d4110092ac0b2afc8d
zyrill <lautrezov@gmail.com>
2025-03-12 23:52:36 +0800

    update

---
+++ main.js
@@ -0,0 +1,74 @@
+var count = 0;
+var exitCount = 0;
+
+function showBox(){
+  const alertBox = document.getElementById("alertBox");
+  alertBox.style.display = "flex"
+  alertBox.style.animation = "show 1s forwards";
+  
+  setTimeout(() => {
+    alertBox.style.animation = "hide 1s forwards";
+    setTimeout(() => {
+      alertBox.style.display = "none";
+    }, 500)
+  }, 3000)
+}
+
+var image = document.getElementById("image");
+let kenshie = document.getElementById("kenshie").onclick = function(){
+  let word = document.getElementById("kenshie");
+  count++;
+  if(count == 1){
+    image.src = "images/shy.gif";
+    word.textContent = "oh no";
+    
+  }
+  else if(count == 2){
+    image.src = "images/accepted.gif";
+    word.textContent = "uhh";
+  }
+  else if(count == 3){
+    alert("huh??");
+  }
+  else if(count == 4){
+    alert("system crash")
+    word.textContent = "continue";
+  }
+  else if(count == 5){
+    window.location.href = "main.html";
+  }
+}
+let jee = document.getElementById("jee").onclick = function(){
+  exitCount++;
+  let exitWord = document.getElementById("jee");
+  let kenshiejee = document.getElementById("kenshiejee");
+  if(exitCount == 1){
+    image.src = "images/rejected.gif";
+    exitWord.textContent = "uhh...";
+    kenshiejee.textContent = "nooo! not allowed"
+    showBox();
+  }
+  else if(exitCount == 2){
+    exitWord.textContent = "wait what?";
+    kenshiejee.textContent = "access denied!"
+    showBox();
+  }
+  else if(exitCount == 3){
+    kenshiejee.textContent = "rejected. try again never";
+    exitWord.textContent = "ouch";
+    showBox();
+  }
+  else if(exitCount == 4){
+    kenshiejee.textContent = "error 404: chance not found";
+    exitWord.textContent = "bruh";
+    showBox();
+  }
+  else if(exitCount == 5){
+    kenshiejee.textContent = "application denied";
+    exitWord.textContent = "crying";
+    showBox();
+  }
+  else if(exitCount == 6){
+    window.location.href = "https://vt.tiktok.com/ZSMsQ31Tn/";
+  }
+}
